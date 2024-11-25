815ef9789b381719354821d015eed703e2e27217
Abdallah Ibrahim <abdallahnasr505@gmail.com>
2024-11-25 13:32:31 +0200

    test

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ from feature_extractor import FeatureExtractor
 from datetime import datetime
 from flask import Flask, request, render_template
 from pathlib import Path
-import itertools
-
 
 app = Flask(__name__)
 
@@ -18,11 +16,13 @@ for feature_path in Path("./static/feature").glob("*.npy"):
     img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
 features = np.array(features)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['query_img']
 
+        # Save query image
         img = Image.open(file.stream)  # PIL image
         uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
         img.save(uploaded_img_path)
@@ -31,12 +31,14 @@ def index():
         query = fe.extract(img)
         dists = np.linalg.norm(features-query, axis=1)  # L2 distances to features
         ids = np.argsort(dists)[:30]  # Top 30 results
-        scores = [(dists[id], img_paths[id]) for id in itertools.islice(ids, 15)]
+        scores = [(dists[id], img_paths[id]) for id in ids]
 
-        return render_template('index.html',query_path=uploaded_img_path,scores=scores)
+        return render_template('index.html',
+                               query_path=uploaded_img_path,
+                               scores=scores)
     else:
-        return render_template('index.html')    
+        return render_template('index.html')
+
 
 if __name__=="__main__":
-
-    app.run(host="0.0.0.0",debug=True,port=9600)
+    app.run(debug=True, host="127.0.0.1", port=5000)
